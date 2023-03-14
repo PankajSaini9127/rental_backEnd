ae0bccafc58e7c7a38acfcf95952cbd7602c4a41
Pankaj Saini <Pankajsolanki6008@gmail.com>
2023-03-14 14:24:46 +0530

    initial commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const db = require('./data/db')
 
 const app = express();
 
+const Port = process.env.PORT || 8080
+
 const cors = require("cors");
 
 app.use(cors());
@@ -15,47 +17,7 @@ app.post('/admin',)
 
 app.use('/api/admin',require('./Routes/AdminPanel'))
 
-// // login API
-app.post("/", (req, res) => {
-  db.from(`admin-login`).select('*').where(req.body)
-  .then(result=>res.send(result))
-  .catch(err=>res.send(err))
-});
 
-//admin registration on DB
-app.post("/adminReg", (req, res) => {
-  db(`admin-login`).insert(req.body)
-  .then(result=>res.send(result))
-  .catch(err=>res.send(err))
-});
-
-
-
-app.delete("/adminReg:id", (req, res) => {
-  db(`admin-login`).where("id", req.params.id).del()
-  .then(result=> res.Status(301).send(result))
-  .catch(err=>res.send(err))
-});
-
-
- //dashboard api
-app.get("/dashboard", (req, res) => {
-  db.from('dashboard').select('*')
-    .then(result=>res.send(result))
-    .catch(err=>res.send(err))
-  });
-
-
-  // agreement form data post into db
-app.post('/api/agreement', async(req,res)=>{
-  try {
-    const result = await db('agreements').insert(req.body.data)
-    res.status(201).send({message:"Submited"})
-  } catch (error) {
-    res.status(404).send(error)
-  }
-   
-})
 
 
 
