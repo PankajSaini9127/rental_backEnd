42f3f191654e8a44bf59ba9927266a169c3e8980
YASH1730 <yashwantsahu3002@gmail.com>
2023-05-02 14:09:47 +0530

    fix :: in MIS

--- Routes/MisReports.js
+++ Routes/MisReports.js
@@ -5,6 +5,9 @@ const {
   get_Rental_Onboarding_All_Status,
   get_Rental_Onboarding_Deposited,
   get_Rent_Paid_Schedule,
+  get_No_Of_Agreements,
+  get_Monthly_Rent,
+  get_Monthly_Deposit,
 } = require("../controller/MisReportControls");
 
 const router = express.Router();
@@ -24,5 +27,11 @@ router
   .get(get_Rental_Onboarding_Deposited);
 
 router.route("/rent-paid-schedule").get(get_Rent_Paid_Schedule);
+
+router.route("/no-of-agreements").get(get_No_Of_Agreements);
+
+router.route("/monthly-rent").get(get_Monthly_Rent);
+
+router.route("/monthly-deposit").get(get_Monthly_Deposit);
 
 module.exports = router;
